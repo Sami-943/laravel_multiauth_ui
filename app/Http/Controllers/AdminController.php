417aa99477f8cff366f7ca712cfb4fba4bead7eb
Sami Akhter <samibasit1998@gmail.com>
2023-02-18 16:43:58 +0500

    no commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use Validator;
+
+class AdminController extends Controller
+{
+    public function authenticate(Request $request){
+            $this->validate($request,[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password],$request->get('remember'))) {
+            return redirect()->route('admin.dashboard');
+        } else {
+            session()->flash('error','Enter valid email and password');
+            return back()->withInput($request->only('email'));
+        }
+        
+    }
+    public function logout(){
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+}
